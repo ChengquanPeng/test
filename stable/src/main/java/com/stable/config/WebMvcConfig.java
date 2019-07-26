112558a3c76f046d48377fafd5af50c67afb3293
pengchengquan <348000443@qq.com>
2019-07-26 10:10:56 +0800

    test

--- stable/src/main/java/com/stable/config/WebMvcConfig.java
+++ stable/src/main/java/com/stable/config/WebMvcConfig.java
@@ -4,10 +4,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -17,6 +19,12 @@ import com.stable.utils.MyBeanSerializerModifier;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
+	@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
 	@Bean
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
