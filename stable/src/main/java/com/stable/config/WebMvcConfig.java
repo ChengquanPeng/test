65d9698ed0f71297e6da15cae3e56165655f2dbf
pengchengquan <348000443@qq.com>
2019-09-17 18:41:03 +0800

    test

--- stable/src/main/java/com/stable/config/WebMvcConfig.java
+++ stable/src/main/java/com/stable/config/WebMvcConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -19,6 +20,12 @@ import com.stable.utils.MyBeanSerializerModifier;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	}
+
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
