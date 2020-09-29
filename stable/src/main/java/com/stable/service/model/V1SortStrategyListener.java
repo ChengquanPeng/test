d86dd9b4adfac6f7edb2903f14f990b328af5247
pengchengquan <348000443@qq.com>
2020-09-29 11:37:49 +0800

    test

--- stable/src/main/java/com/stable/service/model/V1SortStrategyListener.java
+++ stable/src/main/java/com/stable/service/model/V1SortStrategyListener.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -33,19 +31,19 @@ public class V1SortStrategyListener implements StrategyListener {
 	private String endder = "</table><script type='text/javascript' src='/tkhtml/static/addsinaurl.js'></script>";
 
 	// 不OK List
-	private List<ModelContext> mcs = Collections.synchronizedList(new LinkedList<ModelContext>());
+	// private List<ModelContext> mcs = Collections.synchronizedList(new
+	// LinkedList<ModelContext>());
 
 	// OK List
 	List<ModelV1> saveList = Collections.synchronizedList(new LinkedList<ModelV1>());
 
 	// OK和不OK结果
-	private Map<String, String> result = new ConcurrentHashMap<String, String>();
+	// private Map<String, String> result = new ConcurrentHashMap<String, String>();
 
 	// 均线横盘突破,放量上涨:
 	// 价格排除:x涨太多;x上影线;x大幅高开低走(不包括收盘5日线上);
 	// 量排除: x换手率太高或者太低; 放量超过30日均量50%
 
-	private int treadeDate;
 	private CodeModelService codeModelService;
 
 	private void setDetail(StringBuffer detailDesc, String desc) {
@@ -76,9 +74,17 @@ public class V1SortStrategyListener implements StrategyListener {
 				if (lineAvgPrice.feedData()) {
 					if (lineAvgPrice.isWhiteHorseV2()) {
 						boolean b6 = linePrice.checkPriceBack6dayWhitTodayV2();// 5.回调过超10%
-						if (b6 && linePrice.oneYearCheck(1, mc.getCode(), mc.getDate())) {// 一年未涨
-							isOk = true;
+						if (b6) {// 一年未涨
+							if (linePrice.oneYearCheck(1, mc.getCode(), mc.getDate())) {// 一年未涨
+								isOk = true;
+							} else {
+								dropOutMsg = "未一年未涨";
+							}
+						} else {
+							dropOutMsg = "未回调过超10%";
 						}
+					} else {
+						dropOutMsg = "非白马";
 					}
 				} else {
 					isOk = false;
@@ -140,16 +146,16 @@ public class V1SortStrategyListener implements StrategyListener {
 				mv.setPriceIndex(mc.getPriceIndex());
 				mv.setScore(baseScore + strongScore + pgmScore + wayScore + gnScore);
 
-				result.put(mv.getCode(), detailDesc.toString());
+				// result.put(mv.getCode(), detailDesc.toString());
 				saveList.add(mv);
 			} else {
 				log.info("code={},dropOutMsg={}", mc.getCode(), dropOutMsg);
-				result.put(mv.getCode(), dropOutMsg);
-				mcs.add(mc);
+				// result.put(mv.getCode(), dropOutMsg);
+				// mcs.add(mc);
 			}
 		} else {
-			result.put(mv.getCode(), mc.getBaseDataOk());
-			mcs.add(mc);
+			// result.put(mv.getCode(), mc.getBaseDataOk());
+			// mcs.add(mc);
 		}
 	}
 
@@ -157,7 +163,6 @@ public class V1SortStrategyListener implements StrategyListener {
 
 	public V1SortStrategyListener(int date, CodeModelService codeModelService) {
 		this.codeModelService = codeModelService;
-		this.treadeDate = date;
 		String[] s = { "序号", "代码", "简称", "日期", "综合评分", "基本面评分", "短期强势", "主力行为" };
 		for (int i = 0; i < s.length; i++) {
 			header += this.getHTMLTH(s[i]);
@@ -208,30 +213,30 @@ public class V1SortStrategyListener implements StrategyListener {
 		// fulshToFile2();
 	}
 
-	protected void fulshToFile2() {
-		StockBasicService sbs = SpringUtil.getBean(StockBasicService.class);
-		SpringConfig efc = SpringUtil.getBean(SpringConfig.class);
-		String filepath2 = efc.getModelV1SortFloderDesc() + "sort_v1_dropout_" + treadeDate + ".html";
-		StringBuffer sb2 = new StringBuffer(
-				"<table border='1' cellspacing='0' cellpadding='0'><tr><th>seq</th><th>code</th><th>名称</th><th>分数</th><th>均线排列(20)</th><th>原因</th></tr>"
-						+ FileWriteUitl.LINE_FILE);
-		log.info("mcs:size:" + mcs.size());
-		for (int i = 0; i < mcs.size(); i++) {
-			// log.info("currIndex:{},totoal:{}", i, mcs.size());
-			ModelContext mc = mcs.get(i);
-			String code = mc.getCode();
-			sb2.append("<tr>").append(getHTML(i)).append(getHTML_SN(code))// 代码
-					.append(getHTML(sbs.getCodeName(code)))// 名称
-					.append(getHTML(mc.getScore()))// 分数
-					.append(getHTML(mc.isBase30Avg()))// 至少30日均线排列
-					.append(getHTML(result.get(code))).append("</tr>").append(FileWriteUitl.LINE_FILE);// 原因
-		}
-		sb2.append(endder);
-
-		FileWriteUitl fw2 = new FileWriteUitl(filepath2, true);
-		fw2.writeLine(sb2.toString());
-		fw2.close();
-	}
+//	protected void fulshToFile2() {
+//		StockBasicService sbs = SpringUtil.getBean(StockBasicService.class);
+//		SpringConfig efc = SpringUtil.getBean(SpringConfig.class);
+//		String filepath2 = efc.getModelV1SortFloderDesc() + "sort_v1_dropout_" + treadeDate + ".html";
+//		StringBuffer sb2 = new StringBuffer(
+//				"<table border='1' cellspacing='0' cellpadding='0'><tr><th>seq</th><th>code</th><th>名称</th><th>分数</th><th>均线排列(20)</th><th>原因</th></tr>"
+//						+ FileWriteUitl.LINE_FILE);
+//		log.info("mcs:size:" + mcs.size());
+//		for (int i = 0; i < mcs.size(); i++) {
+//			// log.info("currIndex:{},totoal:{}", i, mcs.size());
+//			ModelContext mc = mcs.get(i);
+//			String code = mc.getCode();
+//			sb2.append("<tr>").append(getHTML(i)).append(getHTML_SN(code))// 代码
+//					.append(getHTML(sbs.getCodeName(code)))// 名称
+//					.append(getHTML(mc.getScore()))// 分数
+//					.append(getHTML(mc.isBase30Avg()))// 至少30日均线排列
+//					.append(getHTML(result.get(code))).append("</tr>").append(FileWriteUitl.LINE_FILE);// 原因
+//		}
+//		sb2.append(endder);
+//
+//		FileWriteUitl fw2 = new FileWriteUitl(filepath2, true);
+//		fw2.writeLine(sb2.toString());
+//		fw2.close();
+//	}
 
 	private String getHTML(Object text) {
 		return "<td>" + text + "</td>";
