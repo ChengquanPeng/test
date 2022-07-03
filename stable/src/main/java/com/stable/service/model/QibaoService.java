d4b48a8a6457137358422b3ff1ec089b609d354d
pengchengquan <348000443@qq.com>
2022-07-04 00:30:20 +0800

    d

--- stable/src/main/java/com/stable/service/model/QibaoService.java
+++ stable/src/main/java/com/stable/service/model/QibaoService.java
@@ -40,20 +40,20 @@ public class QibaoService {
 //		 000678-20220610
 //		 000025-20220519
 //		 000017-20220526
-//		 000582-20211115
+//		 000582-20220331
 //		 600798-20201113
-	// 大旗形
-//		String[] codes = { "002612", "002900", "600789", "000678", "000025", "000017","000582" };
-//		int[] dates = { 20200527, 20210315, 20200115, 20220610, 20220519, 20220526,20211115 };
-	// 小旗形
-//		String[] codes = { "000563", "601515" };
-//		int[] dates = { 20220701, 20220701 };
-	// 十字星
+		// 大旗形
+//		String[] codes = { "002612", "002900", "600789", "000678", "000025", "000017", "600798" };
+//		int[] dates = { 20200527, 20210315, 20200115, 20220610, 20220519, 20220526, 20201113 };
+		// 小旗形
+//		String[] codes = { "000582", "000563", "601515" };
+//		int[] dates = { 20220331, 20220701, 20220701 };
+		// 十字星
 //		String[] codes = { "002752" };
 //		int[] dates = { 20211115 };
-
-//		String[] codes = { "600798" };
-//		int[] dates = { 20201113 };
+////
+//		String[] codes = { "000582" };
+//		int[] dates = { 20220331 };
 //
 //		for (int i = 0; i < codes.length; i++) {
 //			String code = codes[i];
@@ -124,8 +124,7 @@ public class QibaoService {
 				}
 			}
 		}
-
-		boolean dibq1 = dibuqixing(newOne, res);// 2.是否底部旗形(旗形过滤)：1.在底部旗形，2.旗形前没怎么涨,3.是否更优的旗形:进一步判断底部旗形
+		boolean dibq1 = dibuPreChk(newOne, res, true);// 2.是否底部旗形(旗形过滤)：1.在底部旗形，2.旗形前没怎么涨,3.是否更优的旗形:进一步判断底部旗形
 		if (dibq1) {
 			if (newOne.getDibuQixing() == 0) {
 				qx.append(stockBasicService.getCodeName2(newOne.getCode())).append(",");
@@ -172,7 +171,7 @@ public class QibaoService {
 			d2tian = chk;
 		}
 
-		boolean dibq2 = dibuqixing2(newOne, res);// 2.是否底部旗形(旗形过滤)：1.在底部旗形，2.旗形前没怎么涨,3.是否更优的旗形:进一步判断底部旗形
+		boolean dibq2 = dibuPreChk(newOne, res, true);// 2.是否底部旗形(旗形过滤)：1.在底部旗形，2.旗形前没怎么涨,3.是否更优的旗形:进一步判断底部旗形
 		if (dibq2) {
 			if (newOne.getDibuQixing2() == 0) {
 				qx.append(stockBasicService.getCodeName2(newOne.getCode())).append(",");
@@ -199,7 +198,7 @@ public class QibaoService {
 		// 1. 第一天中阳线,3-6个点,,第二天十字星或者小影线
 		// 2.前5天涨幅低于2%
 		TradeHistInfoDaliy d2tian = list.get(0);
-		TradeHistInfoDaliy res = null;
+		QiBaoInfo res = null;
 		double moniHigh = 0.0;
 		for (int i = 1; i < list.size(); i++) {
 			TradeHistInfoDaliy chk = list.get(i);
@@ -208,7 +207,10 @@ public class QibaoService {
 				// 收影线或者10字星
 				if (3.0 <= chk.getTodayChangeRate() && chk.getTodayChangeRate() <= 7.0) {// 第一天中阳线,3-6个点
 					if (isShizixing(newOne.getCode(), chk.getDate())) {
-						res = chk;
+						res = new QiBaoInfo();
+						res.setDate(chk.getDate());
+						res.setYesterdayPrice(chk.getYesterdayPrice());
+						res.setPrice(chk.getHigh());
 						moniHigh = (chk.getHigh() > d2tian.getHigh()) ? chk.getHigh() : d2tian.getHigh();
 						break;
 					}
@@ -218,7 +220,8 @@ public class QibaoService {
 		}
 
 		boolean isOk = false;
-		if (res != null) {// chkdate之后的验证
+		if (res != null) {
+			// chkdate之后的验证
 			boolean preChkOk = true;
 			int tims = 0;
 			for (TradeHistInfoDaliy nf : list) {// 倒序循环
@@ -231,11 +234,15 @@ public class QibaoService {
 							break;
 						}
 					}
-					if (nf.getClosed() > res.getHigh()) {
+					if (nf.getClosed() > res.getPrice()) {// CHK 最高价
 						preChkOk = false;
 						break;
 					}
 				}
+			}
+			// chkdate 之前的验证
+			if (preChkOk) {
+				preChkOk = this.dibuPreChk(newOne, res, false);
 			}
 			isOk = preChkOk;
 		}
@@ -281,11 +288,11 @@ public class QibaoService {
 	}
 
 	// CHK Date 之前的验证
-	private boolean dibuqixing(CodeBaseModel2 newOne, QiBaoInfo res) {
+	private boolean dibuPreChk(CodeBaseModel2 newOne, QiBaoInfo res, boolean isQx) {
 		if (res == null) {
 			return false;
 		}
-		// 排除:拉高后的旗形：旗形日前1个月涨幅低于15%
+		// 排除1:拉高后的旗形：旗形日前<1>个月涨幅低于15%
 		List<TradeHistInfoDaliy> list = daliyTradeHistroyService.queryListByCodeWithLastQfq(newOne.getCode(), 0,
 				res.getDate(), EsQueryPageUtil.queryPage45, SortOrder.DESC);
 		double low = Integer.MAX_VALUE;
@@ -301,7 +308,7 @@ public class QibaoService {
 				return false;// 涨幅太大
 			}
 		}
-		// 排除:拉高后的旗形：旗形日前2个月涨幅低于20%
+		// 排除2:拉高后的旗形：旗形日前<2>个月涨幅低于20%
 		low = Integer.MAX_VALUE;
 		for (TradeHistInfoDaliy nf : list) {
 			if (nf.getClosed() < low) {
@@ -315,7 +322,9 @@ public class QibaoService {
 			}
 		}
 
-		// 排除2：半年内已经拉高过的旗形，之前没有连续20个交易日涨幅超过50%的
+		// 排除2：半年内已经拉高过的
+		// 1.之前没有《连续10个交易日》涨幅超过35%的
+		// 1.之前没有《连续20个交易日》涨幅超过50%的
 		List<TradeHistInfoDaliy> list2 = daliyTradeHistroyService.queryListByCodeWithLastQfq(newOne.getCode(), 0,
 				res.getDate(), EsQueryPageUtil.queryPage120, SortOrder.DESC);
 		List<TradeHistInfoDaliy> tmpl = new LinkedList<TradeHistInfoDaliy>();
@@ -367,161 +376,44 @@ public class QibaoService {
 			}
 		}
 
-		// 排除3:排除退市股票
-		if(stTuiShi(newOne)) {
-			return false;
-		}
-
-		// log.info(list2.get(list2.size() - 1).getDate());
-		// log.info(list2.get(0).getDate());
-		int up4 = 0;
-		for (int i = 1; i <= 10; i++) {// 排除大涨的哪天
-			TradeHistInfoDaliy nf = list.get(i);
-			if (nf.getTodayChangeRate() >= 7.5) {// 大涨直接排除
-				log.info("之前已经大涨7%");
-				return false;
-			}
-			// 上涨趋势前上涨，量大于这天，不要
-			if (nf.getLow() < res.getLow() && nf.getTodayChangeRate() > 0 && nf.getVolume() > res.getVol()) {
-				log.info("之前已经上涨放量");
-				return false;
-			}
-			if (nf.getTodayChangeRate() >= 4) {// 涨幅超过4个点，++
-				up4++;
-			}
-		}
-		// 前面几个交易日波动必须小于4个点（只允许1-2次）
-		if (up4 <= 1) {
-			return true;
-		}
-		log.info("前面几个交易日波动4%超1");
-		return false;
-	}
-
-	/**
-	 * CHK Date 之前的验证
-	 */
-	private boolean dibuqixing2(CodeBaseModel2 newOne, QiBaoInfo res) {
-		if (res == null) {
-			return false;
-		}
-		// 底部小票
-		// 排除:拉高后的旗形：旗形日前1个月涨幅低于15%
-		List<TradeHistInfoDaliy> list = daliyTradeHistroyService.queryListByCodeWithLastQfq(newOne.getCode(), 0,
-				res.getDate(), EsQueryPageUtil.queryPage45, SortOrder.DESC);
-		double low = Integer.MAX_VALUE;
-		for (int i = 1; i <= 20; i++) {
-			TradeHistInfoDaliy nf = list.get(i);
-			if (nf.getClosed() < low) {
-				low = nf.getClosed();// 最小
-			}
-		}
-		if (low < res.getYesterdayPrice()) {
-			if (CurrencyUitl.cutProfit(low, res.getYesterdayPrice()) > 15) {// 本身已经大涨了，之前就不能涨太多
-				log.info("本身已经大涨了，之前就不能涨太多1");
-				return false;// 涨幅太大
-			}
-		}
-		// 排除:拉高后的旗形：旗形日前2个月涨幅低于20%
-		low = Integer.MAX_VALUE;
-		for (TradeHistInfoDaliy nf : list) {
-			if (nf.getClosed() < low) {
-				low = nf.getClosed();// 最小
-			}
-		}
-		if (low < res.getYesterdayPrice()) {
-			if (CurrencyUitl.cutProfit(low, res.getYesterdayPrice()) > 20) {// 本身已经大涨了，之前就不能涨太多
-				log.info("本身已经大涨了，之前就不能涨太多2");
-				return false;// 涨幅太大
-			}
-		}
-
-		// 排除2：半年内已经拉高过的旗形，之前没有连续20个交易日涨幅超过50%的
-		List<TradeHistInfoDaliy> list2 = daliyTradeHistroyService.queryListByCodeWithLastQfq(newOne.getCode(), 0,
-				res.getDate(), EsQueryPageUtil.queryPage120, SortOrder.DESC);
-		List<TradeHistInfoDaliy> tmpl = new LinkedList<TradeHistInfoDaliy>();
-		List<TradeHistInfoDaliy> tmpl2 = new LinkedList<TradeHistInfoDaliy>();
-		for (int i = list2.size() - 1; i >= 0; i--) {
-
-			// 10个交易日超过35%的
-			TradeHistInfoDaliy t = list2.get(i);
-			if (tmpl2.size() <= 10) {
-				tmpl2.add(t);
-			}
-			if (tmpl2.size() > 10) {
-				tmpl2.remove(0);
-			}
-			if (tmpl2.size() == 10) {
-				TradeHistInfoDaliy topDate = tmpl2.stream().max(Comparator.comparingDouble(TradeHistInfoDaliy::getHigh))
-						.get();
-				TradeHistInfoDaliy lowDate = tmpl2.stream().min(Comparator.comparingDouble(TradeHistInfoDaliy::getLow))
-						.get();
-				// 上涨趋势
-				if (topDate.getDate() > lowDate.getDate()) {
-					if (CurrencyUitl.cutProfit(lowDate.getLow(), topDate.getHigh()) >= 35) {
-						// 有的是K线是挖坑，有的是拉高，拉高收货回踩后的旗形可能会错过，比如002864
-						log.info("10个交易日超过35%," + lowDate.getDate() + "-" + topDate.getDate());
-						return false;
-					}
-				}
-			}
-
-			// 20个交易日超过50%的
-			if (tmpl.size() <= 20) {
-				tmpl.add(t);
-			}
-			if (tmpl.size() > 20) {
-				tmpl.remove(0);
-			}
-			if (tmpl.size() == 20) {
-				TradeHistInfoDaliy topDate = tmpl.stream().max(Comparator.comparingDouble(TradeHistInfoDaliy::getHigh))
-						.get();
-				TradeHistInfoDaliy lowDate = tmpl.stream().min(Comparator.comparingDouble(TradeHistInfoDaliy::getLow))
-						.get();
-				// 上涨趋势
-				if (topDate.getDate() > lowDate.getDate()) {
-					if (CurrencyUitl.cutProfit(lowDate.getLow(), topDate.getHigh()) >= 50) {
-						log.info("20个交易日超过50%");
-						return false;
-					}
-				}
-			}
-		}
-
+		// 排除3:排除退市股票&ST
 		if (stTuiShi(newOne)) {
 			return false;
 		}
 
-		// log.info(list2.get(list2.size() - 1).getDate());
-		// log.info(list2.get(0).getDate());
-		int up4 = 0;
-		for (int i = 1; i <= 10; i++) {// 排除大涨的哪天
-			TradeHistInfoDaliy nf = list.get(i);
-			if (nf.getTodayChangeRate() >= 7.5) {// 大涨直接排除
-				log.info("之前已经大涨7%");
-				return false;
+		if (isQx) {
+			// 前10个交易
+			int up4 = 0;
+			for (int i = 1; i <= 10; i++) {// 排除大涨的哪天
+				TradeHistInfoDaliy nf = list.get(i);
+				if (nf.getTodayChangeRate() >= 7.5) {// 大涨直接排除
+					log.info("之前已经大涨7%");
+					return false;
+				}
+				// 上涨趋势前上涨，量大于这天，不要
+				if (nf.getLow() < res.getLow() && nf.getTodayChangeRate() > 0 && nf.getVolume() > res.getVol()) {
+					log.info("之前已经上涨放量");
+					return false;
+				}
+				if (nf.getTodayChangeRate() >= 4) {// 涨幅超过4个点，++
+					up4++;
+				}
 			}
-			// 上涨趋势前上涨，量大于这天，不要
-			if (nf.getLow() < res.getLow() && nf.getTodayChangeRate() > 0 && nf.getVolume() > res.getVol()) {
-				log.info("之前已经上涨放量");
-				return false;
+			// 前面几个交易日波动必须小于4个点（只允许1-2次）
+			if (up4 <= 1) {
+				return true;
 			}
-			if (nf.getTodayChangeRate() >= 4) {// 涨幅超过4个点，++
-				up4++;
-			}
-		}
-		// 前面几个交易日波动必须小于4个点（只允许1-2次）
-		if (up4 <= 1) {
+			log.info("前面几个交易日波动4%超1");
+			return false;
+		} else {
 			return true;
 		}
-		log.info("前面几个交易日波动4%超1");
-		return false;
 	}
 
-	// 排除3:排除退市股票
+	// 排除3:排除退市股票&ST
 	private boolean stTuiShi(CodeBaseModel2 newOne) {
 		String name = stockBasicService.getCodeName(newOne.getCode());
-		if (name.startsWith(Constant.TUI_SHI) || name.endsWith(Constant.TUI_SHI)||name.contains("ST")) {
+		if (name.startsWith(Constant.TUI_SHI) || name.endsWith(Constant.TUI_SHI) || name.contains("ST")) {
 			log.info("退市");
 			return true;
 		}
@@ -598,13 +490,15 @@ public class QibaoService {
 									// 3天下跌13.5%以上
 									chkrateupdate = true;// 之前涨幅不能超过默认15,这个宽幅震荡可以20
 								} else {
+									log.info("=====>非十字星或上影线1");
 									return null;
 								}
 							} else {
+								log.info("=====>非十字星或上影线2");
 								return null;
 							}
 						} else {
-							log.info("=====>非十字星或上影线");
+							log.info("=====>非十字星或上影线3");
 							return null;
 						}
 					}
