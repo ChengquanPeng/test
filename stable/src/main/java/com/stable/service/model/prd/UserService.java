b5e7e2081fa0133086c7a3c3ad0572d1c122a07c
pengchengquan <348000443@qq.com>
2022-07-20 17:56:27 +0800

    a

--- stable/src/main/java/com/stable/service/model/prd/UserService.java
+++ stable/src/main/java/com/stable/service/model/prd/UserService.java
@@ -62,15 +62,15 @@ public class UserService {
 
 	public List<UserInfo> getUserListForMonitorS2() {
 		List<UserInfo> r = new LinkedList<UserInfo>();
-		int today = DateUtil.getTodayIntYYYYMMDD();
-		UserInfo q = new UserInfo();
-		q.setS2(today);
-		List<UserInfo> l = this.getList(q, EsQueryPageUtil.queryPage9999);
-		if (l != null) {
-			for (UserInfo u : l) {
-				r.add(u);
-			}
-		}
+//		int today = DateUtil.getTodayIntYYYYMMDD();
+//		UserInfo q = new UserInfo();
+//		q.setS2(today);
+//		List<UserInfo> l = this.getList(q, EsQueryPageUtil.queryPage9999);
+//		if (l != null) {
+//			for (UserInfo u : l) {
+//				r.add(u);
+//			}
+//		}
 		UserInfo myid = new UserInfo();
 		myid.setId(Constant.MY_ID);
 		r.add(myid);
